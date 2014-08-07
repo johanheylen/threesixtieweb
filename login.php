2546a4ae0321e953497dbb030a6082ea98cdda35
leander.dierckx <leander.dierckx@gmail.com>
2014-08-07 08:53:23 +0000

    logo's as an icing on the cake

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 	<label for="username"><?php echo get_text('Username'); ?>: </label><input type="text" name="username" />
 	<br />
 	<label for="password"><?php echo get_text('Password'); ?>: </label><input type="password" name="password" />
-
+<!-- consider adding a logo here dnsbelgium.png (please ask for the file)-->
 </form>
 
 <?php require('includes/footer.php'); ?>
