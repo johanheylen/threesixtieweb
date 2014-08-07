08a48ed09a68a022df58f5599014c72adebc755c
leander.dierckx@gmail.com <leander.dierckx@gmail.com@38e876b8-0253-0498-fc36-39b8a37de659>
2014-08-07 08:49:49 +0000

    some database comments about permissions (to be considered later on)

--- trunk/core/database/connect.php
+++ trunk/core/database/connect.php
@@ -1,5 +1,9 @@
 <?php
 	$connect_error = 'Sorry, we\'re experiencing connection problems.';
 	mysql_connect('localhost', 'root', '') or die($connect_error);
+	/* consider using minimal privileges for app : */
+	/* dba user = root (all privs on all dbs) */
+	/* admin user = create drop constraint ... */
+	/* app user = insert update delete select (this one we need in the php ) */
 	mysql_select_db('threesixtyweb') or die($connect_error);
 ?>
