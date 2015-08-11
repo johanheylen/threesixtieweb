83e707dcdd425e2d68c31068d34469ffc2b59fde
johanh <johanh@dns.be>
2015-08-11 11:50:09 +0200

    default database connect string

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,7 +1,7 @@
 <?php
 $connect_error = 'Sorry, we\'re experiencing connection problems with the database.';
-/* mysql_connect('localhost', 'root', '') or die($connect_error); */
-mysql_connect('127.0.0.1', 'threesixtyweb', 'threesixtywebapp') or die($connect_error);
+/* mysql_connect('/var/lib/mysql/mysqld1/mysqld1.sock', 'root', '') or die($connect_error);*/
+mysql_connect('127.0.0.1', 'root', '') or die($connect_error);
 /* consider using minimal privileges for app : */
 /* dba user = root (all privs on all dbs) */
 /* admin user = create drop constraint ... */
